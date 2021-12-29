6ed3d8b1e8f18795fc674ce465b679a306330129
Łukasz Klekner <lukaszklekner@gmail.com>
2021-12-29 20:24:59 +0100

    creates Status component and types its props

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,11 @@
 import './App.css'
-import { Greet } from './components/Greet'
-import { Person } from './components/Person'
-import { PersonList } from './components/PersonList'
+import { Status } from './components/Status'
 
 function App() {
-  const name = {
-    first: 'Łukasz',
-    last: 'Klekner',
-  }
-
-  const names = [
-    {
-      first: 'Łukasz',
-      last: 'Klekner',
-    },
-    {
-      first: 'Metusz',
-      last: 'Klekner',
-    },
-    {
-      first: 'Weronika',
-      last: 'Klekner',
-    },
-  ]
   return (
     <div className='App'>
       React - TypeScript
-      <Greet name='Łukasz' countMessages={100} isLogged />
-      <Person name={name} />
-      <PersonList names={names} />
+      <Status status='loading' />
     </div>
   )
 }
---
+++ src/components/Status.tsx
@@ -0,0 +1,21 @@
+type StatusProps = {
+  status: 'loading' | 'success' | 'error'
+}
+
+export const Status = ({ status }: StatusProps) => {
+  let message
+
+  if (status === 'loading') {
+    message = 'Loading...'
+  } else if (status === 'success') {
+    message = 'Data fetched successfully!'
+  } else {
+    message = 'Error fetching data'
+  }
+
+  return (
+    <div>
+      <h2>Status = {message}</h2>
+    </div>
+  )
+}
