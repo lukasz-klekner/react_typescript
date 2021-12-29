59084f6bb4e7a35b5e8128a8d169c3af484ba553
Łukasz Klekner <lukaszklekner@gmail.com>
2021-12-29 21:11:06 +0100

    creates Container component and types its props

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import './App.css'
-import { Button } from './components/Button'
-import { Input } from './components/Input'
+import { Container } from './components/Container'
 
 function App() {
   return (
     <div className='App'>
       React - TypeScript
-      <Button
-        handleClick={(event, id) => console.log('Clicked me!', event, id)}
-      />
-      <Input
-        value=''
-        handleChange={(event) => console.log(event.target.value)}
-      />
+      <Container styles={{ border: '1px solid black', padding: '1rem' }} />
     </div>
   )
 }
