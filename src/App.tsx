7643b183a03370c0ffdfe65b49fcdcdafb102675
Łukasz Klekner <lukaszklekner@gmail.com>
2021-12-29 21:03:47 +0100

    creates Input component and types its props

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { Button } from './components/Button'
+import { Input } from './components/Input'
 
 function App() {
   return (
@@ -7,6 +8,10 @@ function App() {
       React - TypeScript
       <Button
         handleClick={(event, id) => console.log('Clicked me!', event, id)}
+      />
+      <Input
+        value=''
+        handleChange={(event) => console.log(event.target.value)}
       />
     </div>
   )
