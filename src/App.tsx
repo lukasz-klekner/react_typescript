5e2c14f1dec842fe23221a99cbb41d31579020aa
Łukasz Klekner <lukaszklekner@gmail.com>
2022-01-01 11:46:55 +0100

    creates DomRef component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import './App.css'
-import { User } from './components/context/User'
-import { UserContextProvider } from './components/context/UserContext'
+import { DomRef } from './components/ref/DomRef'
 
 function App() {
   return (
     <div className='App'>
       React - TypeScript
-      <UserContextProvider>
-        <User />
-      </UserContextProvider>
+      <DomRef />
     </div>
   )
 }
